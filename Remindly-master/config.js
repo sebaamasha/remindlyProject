4ce5163ec143079a7d77526afa068a9a1c32d5ad
sebaamasha <sebaama96@gmail.com>
2025-10-07 10:55:08 +0300

    "Move Google API key to .env and remove hard-coded key"

--- Remindly-master/config.js
+++ Remindly-master/config.js
@@ -1,6 +1,6 @@
 const config = {
     SERVER_API: 'https://remindlyserver.onrender.com/api',
-    GOOGLE_API: 'AIzaSyAp2CByzchy61Z_OQxvuTRRwc3mUInW0RE',
+    GOOGLE_API: 'process.env.GOOGLE_API',
 };
 
 export default config;
